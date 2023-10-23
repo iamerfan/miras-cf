4dd10171dd4b368eff84d93ee1ac04c58da9af08
Erfan <erfanhosseinizadeh@gmail.com>
2023-10-23 16:59:36 +0330

    changes

--- next.config.js
+++ next.config.js
@@ -1,11 +1,2 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://miras-server.vercel.app/api/:path*",
-      },
-    ];
-  },
-};
+module.exports = {};
